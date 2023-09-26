a5bcfeaf5d0d4c8a1dd24f4619cd8e75b91f4819
chaudhary1337 <tanishqchaudhary511@gmail.com>
2023-09-26 12:51:29 +0530

    playing with locks

--- experiments/lock.go
+++ experiments/lock.go
@@ -1,36 +1,36 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"sync"
-)
+// import (
+// 	"fmt"
+// 	"sync"
+// )
 
-func miau() {
-	fmt.Println("miau")
-}
+// func miau() {
+// 	fmt.Println("miau")
+// }
 
-func main() {
-	counter := 0
-	var mu sync.Mutex
-	var wg sync.WaitGroup
+// func main() {
+// 	counter := 0
+// 	var mu sync.Mutex
+// 	var wg sync.WaitGroup
 
-	for i := 0; i < 1000; i++ {
-		wg.Add(1)
-		go func(){
-			defer wg.Done()
-			
-			mu.Lock()
-			defer mu.Unlock()
-			counter++
+// 	for i := 0; i < 1000; i++ {
+// 		wg.Add(1)
+// 		go func(){
+// 			defer wg.Done()
 
-			miau()
-		}()
-	}
+// 			mu.Lock()
+// 			defer mu.Unlock()
+// 			counter++
 
-	mu.Lock()
-	end := counter
-	mu.Unlock()
-	
-	wg.Wait()
-	fmt.Println(end, counter)
-}
+// 			miau()
+// 		}()
+// 	}
+
+// 	mu.Lock()
+// 	end := counter
+// 	mu.Unlock()
+
+// 	wg.Wait()
+// 	fmt.Println(end, counter)
+// }
